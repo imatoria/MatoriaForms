560678179fa5af16190432b955c7f7d727cb837a
Praveen Matoria <praveen.matoria@gmail.com>
2024-05-16 12:09:19 +0530

    Add project files.

---
+++ MatoriaForms.Api/Controllers/CaptchaController.cs
@@ -0,0 +1,35 @@
+﻿using Lazy.Captcha.Core;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace MatoriaForms.Api.Controllers;
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class CaptchaController(ICaptcha captcha, IHttpContextAccessor httpContextAccessor) : ControllerBase {
+
+    public readonly string SessionSuffix = "Simple";
+
+    // GET: api/captcha/get
+    [HttpGet("{captchaId}")]
+    [ActionName("get")]
+    public ActionResult<string> Get(string captchaId) {
+        var info = captcha.Generate(captchaId);
+        return Ok(info.Base64);
+    }
+    // GET: api/captcha/get-image
+    [HttpGet("{captchaId}")]
+    [ActionName("get-image")]
+    public ActionResult<CaptchaResponse> GetImage(string captchaId) {
+        var info = captcha.Generate(captchaId);
+        var stream = new MemoryStream(info.Bytes);
+
+        return File(stream, "image/gif");
+    }
+
+    // POST: api/captcha/verify
+    [HttpGet()]
+    [ActionName("verify")]
+    public bool Verify(string captchaId, string captchaCode) {
+        return captcha.Validate(captchaId, captchaCode, false, false);
+    }
+}
---
+++ MatoriaForms.Api/Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddCaptcha(builder.Configuration);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => {
+            builder.WithOrigins(["https://m-form.runasp.net", "http://localhost:5500"]) // Add the allowed origin(s)
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+//}
+
+app.UseCors("AllowSpecificOrigin");
+
+//app.UseSession();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
